7a2a4aebb87c7c80d4af6b4723c75b87830f8856
Daniel Sanchez <danxschz@gmail.com>
2022-09-30 19:25:35 -0400

    Refactor app.ts and set up mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import createError from 'http-errors';
-
 import express, { RequestHandler, ErrorRequestHandler  } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
@@ -9,57 +8,45 @@ import mongoose from 'mongoose';
 import indexRouter from './routes/index';
 import collectionsRouter from './routes/collections';
 
-class App {
-  public app: express.Application;
+const app = express();
 
-  constructor() {
-    this.app = express();
-    this.config();
-    this.routerSetup();
-    this.errorHandler();
-  }
+// Mongoose setup
+const mongoDB = `mongodb+srv://danxschz:${process.env.MONGODB_PASSWORD}@portfolio.clt5bgx.mongodb.net/store_app?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error:"));
 
-  private config() {
-    // view engine setup
-    this.app.set('views', path.join(__dirname, 'views'));
-    this.app.set('view engine', 'jade');
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-    this.app.use(logger('dev'));
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(cookieParser());
-    this.app.use(express.static(path.join(__dirname, 'public')));
-  }
+// Middleware setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  private routerSetup() {
-    this.app.use('/', indexRouter);
-    this.app.use('/collections', collectionsRouter);
-  }
+// Routes setup
+app.use('/', indexRouter);
+app.use('/collections', collectionsRouter);
 
-  private errorHandler() {
-    // catch 404 and forward to error handler
-    const requestHandler: RequestHandler = function (req, res, next) {
-      next(createError(404));
-    };
-    this.app.use(requestHandler);
+// Catch 404 and forward to error handler
+const requestHandler: RequestHandler = function (req, res, next) {
+  next(createError(404));
+};
+app.use(requestHandler);
 
-    // error handler
-    const errorRequestHandler: ErrorRequestHandler = function (
-      err,
-      req,
-      res,
-      next
-    ) {
-      // set locals, only providing error in development
-      res.locals.message = err.message;
-      res.locals.error = req.app.get("env") === "development" ? err : {};
+// Error handler
+const errorRequestHandler: ErrorRequestHandler = function (err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-      // render the error page
-      res.status(err.status || 500);
-      res.render("error");
-    };
-    this.app.use(errorRequestHandler);
-  }
-}
+  // Render the error page
+  res.status(err.status || 500);
+  res.render("error");
+};
+app.use(errorRequestHandler);
 
-export default new App().app;
+export default app;
